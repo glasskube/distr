090536f64b57752208e08320f8d1206a13603f36
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-11-19 15:13:35 +0100

    chore: add routing with chi

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,25 @@ import (
 	"net/http"
 
 	"github.com/glasskube/cloud/internal/frontend"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	fileServer := http.FileServerFS(frontend.BrowserFS())
+	router := chi.NewRouter()
+	router.Mount("/api", ApiRouter())
+	router.Handle("/*", http.FileServerFS(frontend.BrowserFS()))
+
 	addr := ":8080"
 	fmt.Printf("listen on %v", addr)
-	if err := http.ListenAndServe(addr, fileServer); err != nil {
+	if err := http.ListenAndServe(addr, router); err != nil {
 		panic(err)
 	}
+	fmt.Println("ok bye")
+}
+
+func ApiRouter() chi.Router {
+	router := chi.NewRouter()
+	router.Get("/hello", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusAccepted) })
+	// TODO: add api routes here
+	return router
 }
