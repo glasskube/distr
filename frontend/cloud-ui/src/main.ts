fd1a7780e38bb6e1fc6e2ef6eabf7e994cdf2985
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-11-19 17:07:33 +0100

    chore: add posthog-js

--- frontend/cloud-ui/src/main.ts
+++ frontend/cloud-ui/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import posthog from 'posthog-js';
+
+posthog.init(
+  '', // TODO: add api key
+  {
+    api_host:'https://eu.i.posthog.com',
+    person_profiles: 'identified_only'
+  }
+)
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
