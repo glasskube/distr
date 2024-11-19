301d0497e2284a568055f270c4e129e0c8ec0442
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-11-19 16:44:06 +0100

    fix: always use index.html for not found files

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"net/http"
 
 	"github.com/glasskube/cloud/internal/frontend"
@@ -11,10 +12,10 @@ import (
 func main() {
 	router := chi.NewRouter()
 	router.Mount("/api", ApiRouter())
-	router.Handle("/*", http.FileServerFS(frontend.BrowserFS()))
+	router.Handle("/*", StaticFileHandler())
 
 	addr := ":8080"
-	fmt.Printf("listen on %v", addr)
+	fmt.Printf("listen on %v\n", addr)
 	if err := http.ListenAndServe(addr, router); err != nil {
 		panic(err)
 	}
@@ -26,4 +27,17 @@ func ApiRouter() chi.Router {
 	router.Get("/hello", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusAccepted) })
 	// TODO: add api routes here
 	return router
+}
+
+func StaticFileHandler() http.HandlerFunc {
+	fsys := frontend.BrowserFS()
+	server := http.FileServer(http.FS(fsys))
+	return func(w http.ResponseWriter, r *http.Request) {
+		// check if the requested file exists and use index.html if it does not.
+		if _, err := fs.Stat(fsys, r.URL.Path[1:]); err != nil {
+			http.StripPrefix(r.URL.Path, server).ServeHTTP(w, r)
+		} else {
+			server.ServeHTTP(w, r)
+		}
+	}
 }
