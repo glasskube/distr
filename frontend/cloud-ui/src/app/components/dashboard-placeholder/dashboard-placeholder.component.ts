52f3374c70a29733113b43f1d47852e54540db98
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-11-20 17:02:34 +0100

    feat: add globe component
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- frontend/cloud-ui/src/app/components/dashboard-placeholder/dashboard-placeholder.component.ts
+++ frontend/cloud-ui/src/app/components/dashboard-placeholder/dashboard-placeholder.component.ts
@@ -1,8 +1,15 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {GlobeComponent} from '../globe/globe.component';
+import {DeploymentTargetsService} from '../../services/deployment-targets.service';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-placeholder',
   standalone: true,
+  imports: [GlobeComponent, AsyncPipe],
   templateUrl: './dashboard-placeholder.component.html',
 })
-export class DashboardPlaceholderComponent {}
+export class DashboardPlaceholderComponent {
+  private readonly deploymentTargets = inject(DeploymentTargetsService);
+  public readonly deploymentTargets$ = this.deploymentTargets.list();
+}
