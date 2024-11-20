fca2bc2ddb40fd4f28ec59b1c5e73005eb0c5abf
Philip Miglinci <pmig@glasskube.com>
2024-11-20 15:21:07 +0100

    refactor(cloud-ui): use *ngIf to display alerts, don't use flowbite-angular, remove duplicate code (#12)
    * feat(cloud-ui): add initial flowbite layout
    
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>
    
    * Update frontend/cloud-ui/src/app/app.component.ts
    
    Co-authored-by: Jakob Steiner <kosmoz@users.noreply.github.com>
    
    * refactor: move flowbite init to main.ts, add routing
    
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>
    
    * chore: lint
    
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>
    
    * refactor: use *ngIf to display alerts, don't use flowbite-angular
    
    As the Flowbite Angular restricts us in component styling we decided
    against it
    
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>
    
    ---------
    
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>
    Co-authored-by: Jakob Steiner <kosmoz@users.noreply.github.com>

--- frontend/cloud-ui/src/app/components/side-bar/side-bar.component.ts
+++ frontend/cloud-ui/src/app/components/side-bar/side-bar.component.ts
@@ -1,8 +1,12 @@
 import {Component} from '@angular/core';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
   templateUrl: './side-bar.component.html',
+  imports: [NgIf],
 })
-export class SideBarComponent {}
+export class SideBarComponent {
+  public feedbackAlert = true;
+}
