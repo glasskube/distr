e6916fc7770d5ca704370bad909d37370f114fd0
Philip Miglinci <pmig@glasskube.com>
2024-11-20 13:55:32 +0100

    feat(cloud-ui): add initial flowbite dashboard layout (#5)
    * feat(cloud-ui): add initial flowbite layout
    
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>
    
    * Update frontend/cloud-ui/src/app/app.component.ts
    
    Co-authored-by: Jakob Steiner <kosmoz@users.noreply.github.com>
    
    * refactor: move flowbite init to main.ts, add routing
    
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>
    
    * chore: lint
    
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>
    
    ---------
    
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>
    Co-authored-by: Jakob Steiner <kosmoz@users.noreply.github.com>

--- frontend/cloud-ui/src/app/app.component.ts
+++ frontend/cloud-ui/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import {Component} from '@angular/core';
+import {SideBarComponent} from './components/side-bar/side-bar.component';
+import {NavBarComponent} from './components/nav-bar/nav-bar.component';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [SideBarComponent, NavBarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'cloud';
+  title = 'Glasskube Cloud';
 }
---
+++ frontend/cloud-ui/src/app/components/side-bar/side-bar.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  standalone: true,
+  templateUrl: './side-bar.component.html',
+})
+export class SideBarComponent {}
