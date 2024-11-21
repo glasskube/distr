fc1f81ee2229c2c282387a403630f2b1f804e1c4
Jakob Steiner <kosmoz@users.noreply.github.com>
2024-11-21 15:33:21 +0000

    feat: add create application endpoint (#15)
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- internal/server/router.go
+++ internal/server/router.go
@@ -2,16 +2,24 @@ package server
 
 import (
 	"net/http"
+	"time"
 
 	internalctx "github.com/glasskube/cloud/internal/context"
 	"github.com/glasskube/cloud/internal/handlers"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
 )
 
 func ApiRouter() chi.Router {
 	// TODO for all (most) routes auth middleware
 	router := chi.NewRouter()
-	router.Use(loggerCtxMiddleware, dbCtxMiddleware)
+	router.Use(
+		middleware.RequestID,
+		loggerCtxMiddleware,
+		loggingMiddleware,
+		dbCtxMiddleware,
+	)
 	router.Route("/applications", handlers.ApplicationsRouter)
 	return router
 }
@@ -26,8 +34,25 @@ func dbCtxMiddleware(next http.Handler) http.Handler {
 
 func loggerCtxMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger := getLogger()
+		logger := getLogger().
+			With(zap.String("requestId", middleware.GetReqID(r.Context())))
 		ctx := internalctx.WithLogger(r.Context(), logger)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
+}
+
+func loggingMiddleware(wh http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+		now := time.Now()
+		wh.ServeHTTP(ww, r)
+		elapsed := time.Since(now)
+		logger := internalctx.GetLoggerOrPanic(r.Context())
+		logger.Info("handling request",
+			zap.String("method", r.Method),
+			zap.String("path", r.URL.Path),
+			zap.Int("status", ww.Status()),
+			zap.String("time", elapsed.String()))
+	}
+	return http.HandlerFunc(fn)
 }
