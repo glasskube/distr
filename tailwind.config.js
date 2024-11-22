7fd36cc283c821807d3496bedba11f204f19ed12
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-11-22 14:27:09 +0100

    chore: add missing fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./frontend/cloud-ui/src/**/*.{html,ts}', './frontend/cloud-ui/node_modules/flowbite/**/*.js'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Inter', 'system-ui', 'sans-serif'],
+      display: ['Poppins', 'Inter', 'system-ui', 'sans-serif'],
+    },
   },
   plugins: [require('flowbite/plugin')],
 };
