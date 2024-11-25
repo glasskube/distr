308206741611b20f25ac2bdac6c8a1e07be0d195
Philip Miglinci <pmig@glasskube.com>
2024-11-25 16:15:33 +0100

    refactor: migrate to font awesome icons
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>

--- frontend/cloud-ui/src/app/applications/applications.component.ts
+++ frontend/cloud-ui/src/app/applications/applications.component.ts
@@ -1,25 +1,36 @@
-import {Component} from '@angular/core';
-import {ApplicationsService} from './applications.service';
-import {AsyncPipe, DatePipe} from '@angular/common';
-import {Application} from '../types/application';
-import {Observable} from 'rxjs';
-import {RouterLink} from '@angular/router';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ApplicationsService } from './applications.service';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { Application } from '../types/application';
+import { Observable } from 'rxjs';
+import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faCaretDown, faMagnifyingGlass, faPen, faXmark, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-applications',
   standalone: true,
-  imports: [AsyncPipe, DatePipe, ReactiveFormsModule],
-  templateUrl: './applications.component.html',
+  imports: [AsyncPipe, DatePipe, ReactiveFormsModule, FaIconComponent],
+  templateUrl: './applications.component.html'
 })
 export class ApplicationsComponent {
+
+  magnifyingGlassIcon = faMagnifyingGlass;
+  plusIcon = faPlus;
+  caretDownIcon = faCaretDown;
+  penIcon = faPen;
+  trashIcon = faTrash;
+  xmarkIcon = faXmark;
+
   applications$!: Observable<Application[]>;
   editForm = new FormGroup({
     id: new FormControl(''),
     name: new FormControl('', Validators.required),
     type: new FormControl('', Validators.required),
   });
-  public constructor(private applicationsService: ApplicationsService) {}
+
+  public constructor(private applicationsService: ApplicationsService) { }
 
   ngOnInit() {
     this.applications$ = this.applicationsService.getApplications();
