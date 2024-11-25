900133f901b3af4a06595dcc65528f929ceda5f0
christoph <christoph.enne@glasskube.eu>
2024-11-25 10:04:56 +0100

    feat: applications ui
    Signed-off-by: christoph <christoph.enne@glasskube.eu>

--- frontend/cloud-ui/src/app/applications/applications.service.ts
+++ frontend/cloud-ui/src/app/applications/applications.service.ts
@@ -3,21 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Application } from '../types/application';
 import { combineLatest, map, Observable, scan, shareReplay, startWith, Subject, tap } from 'rxjs';
 
-const accumulate = scan((list: Application[], it: Application) => [it, ...list], [] as Application[]);
-
-type ElementSelectFn<T> = (element: T) => unknown
-type ElementSelector<T> = keyof T | ElementSelectFn<T>
-
-function selectFn<T>(selector: ElementSelector<T>): ElementSelectFn<T> {
-  return selector instanceof Function ? selector : element => element[selector];
+export function distinctById(applications: readonly Application[]): Application[] {
+  return applications.filter((value: Application, index, self) => {
+    return self.findIndex(element => element.id === value.id) === index;
+  });
 }
 
-export function distinctBy<T>(array: readonly T[], selector: ElementSelector<T>): T[] {
-  const select = selectFn(selector);
-  return array.filter((value, index, self) => {
-    const transformedValue = select(value);
-    return self.findIndex(element => select(element) === transformedValue) === index;
-  });
+function compareApplicationsByName(a: Application, b: Application) {
+  return (a.name || '').localeCompare(b.name || '');
 }
 
 @Injectable({
@@ -26,7 +19,7 @@ export function distinctBy<T>(array: readonly T[], selector: ElementSelector<T>)
 export class ApplicationsService {
   private readonly applicationsUrl = '/api/applications';
 
-  private initialApplications: Observable<Application[]>;
+  private readonly initialApplications: Observable<Application[]>;
   private readonly savedApplications = new Subject<Application>();
   private readonly savedApplicationsAccumulated = this.savedApplications.pipe(
     scan((list: Application[], it: Application) => [it, ...list], []), startWith([]))
@@ -36,8 +29,9 @@ export class ApplicationsService {
   constructor(private readonly httpClient: HttpClient) {
     this.initialApplications = this.httpClient.get<Application[]>(this.applicationsUrl);
     this.applications = combineLatest([this.initialApplications, this.savedApplicationsAccumulated]).pipe(
-      map(([initialLs, savedLs]) => distinctBy([...savedLs, ...initialLs], 'id')),
-      shareReplay(1)
+      map(([initialLs, savedLs]) => distinctById([...savedLs, ...initialLs])),
+      map((ls: Application[]) => ls.sort(compareApplicationsByName)),
+      shareReplay(1),
     )
   }
 
