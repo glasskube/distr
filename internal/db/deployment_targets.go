80a0aa010f729d66057f64d765522470220ae663
christoph <christoph.enne@glasskube.eu>
2024-11-28 10:50:19 +0100

    fix: broken insert and update deployment targets
    Signed-off-by: christoph <christoph.enne@glasskube.eu>

--- internal/db/deployment_targets.go
+++ internal/db/deployment_targets.go
@@ -16,7 +16,9 @@ const (
 	deploymentTargetOutputExpr = `
 		dt.id, dt.created_at, dt.name, dt.type,
 		CASE WHEN dt.geolocation_lat IS NOT NULL AND dt.geolocation_lon IS NOT NULL
-		  	THEN (dt.geolocation_lat, dt.geolocation_lon) END AS geolocation,
+		  	THEN (dt.geolocation_lat, dt.geolocation_lon) END AS geolocation
+	`
+	deploymentTargetWithStatusOutputExpr = deploymentTargetOutputExpr + `,
 		CASE WHEN status.id IS NOT NULL
 			THEN (status.id, status.created_at, status.message) END AS current_status
 	`
@@ -32,7 +34,8 @@ const (
 
 func GetDeploymentTargets(ctx context.Context) ([]types.DeploymentTarget, error) {
 	db := internalctx.GetDb(ctx)
-	if rows, err := db.Query(ctx, "SELECT "+deploymentTargetOutputExpr+" "+deploymentTargetFromExpr); err != nil {
+	if rows, err := db.Query(ctx,
+		"SELECT "+deploymentTargetWithStatusOutputExpr+" "+deploymentTargetFromExpr); err != nil {
 		return nil, fmt.Errorf("failed to query DeploymentTargets: %w", err)
 	} else if result, err := pgx.CollectRows(rows, pgx.RowToStructByName[types.DeploymentTarget]); err != nil {
 		return nil, fmt.Errorf("failed to get DeploymentTargets: %w", err)
@@ -44,7 +47,7 @@ func GetDeploymentTargets(ctx context.Context) ([]types.DeploymentTarget, error)
 func GetDeploymentTarget(ctx context.Context, id string) (*types.DeploymentTarget, error) {
 	db := internalctx.GetDb(ctx)
 	rows, err := db.Query(ctx,
-		"SELECT "+deploymentTargetOutputExpr+" "+deploymentTargetFromExpr+" AND dt.id = @id",
+		"SELECT "+deploymentTargetWithStatusOutputExpr+" "+deploymentTargetFromExpr+" AND dt.id = @id",
 		pgx.NamedArgs{"id": id})
 	if err != nil {
 		return nil, fmt.Errorf("failed to query DeploymentTargets: %w", err)
@@ -66,14 +69,14 @@ func CreateDeploymentTarget(ctx context.Context, dt *types.DeploymentTarget) err
 		maps.Copy(args, pgx.NamedArgs{"lat": dt.Geolocation.Lat, "lon": dt.Geolocation.Lon})
 	}
 	rows, err := db.Query(ctx,
-		"INSERT INTO DeploymentTarget (name, type, geolocation_lat, geolocation_lon) "+
+		"INSERT INTO DeploymentTarget AS dt (name, type, geolocation_lat, geolocation_lon) "+
 			"VALUES (@name, @type, @lat, @lon) RETURNING "+
 			deploymentTargetOutputExpr,
 		args)
 	if err != nil {
 		return fmt.Errorf("failed to query DeploymentTargets: %w", err)
 	}
-	result, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[types.DeploymentTarget])
+	result, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByNameLax[types.DeploymentTarget])
 	if err != nil {
 		return fmt.Errorf("could not save DeploymentTarget: %w", err)
 	} else {
@@ -89,13 +92,14 @@ func UpdateDeploymentTarget(ctx context.Context, dt *types.DeploymentTarget) err
 		maps.Copy(args, pgx.NamedArgs{"lat": dt.Geolocation.Lat, "lon": dt.Geolocation.Lon})
 	}
 	rows, err := db.Query(ctx,
-		"UPDATE DeploymentTarget SET name = @name, geolocation_lat = @lat, geolocation_lon = @lon "+
+		"UPDATE DeploymentTarget AS dt SET name = @name, geolocation_lat = @lat, geolocation_lon = @lon "+
 			" WHERE id = @id RETURNING "+
 			deploymentTargetOutputExpr,
 		args)
 	if err != nil {
 		return fmt.Errorf("could not update DeploymentTarget: %w", err)
-	} else if updated, err := pgx.CollectExactlyOneRow(rows, pgx.RowToStructByName[types.DeploymentTarget]); err != nil {
+	} else if updated, err :=
+		pgx.CollectExactlyOneRow(rows, pgx.RowToStructByNameLax[types.DeploymentTarget]); err != nil {
 		return fmt.Errorf("could not get updated DeploymentTarget: %w", err)
 	} else {
 		*dt = updated
