17abdddc22c05e89f742d39f277c5895d095bfe0
christoph <christoph.enne@glasskube.eu>
2024-11-28 10:39:43 +0100

    feat(ui): show status of deployment targets
    Signed-off-by: christoph <christoph.enne@glasskube.eu>

--- frontend/cloud-ui/src/app/types/deployment-target.ts
+++ frontend/cloud-ui/src/app/types/deployment-target.ts
@@ -5,4 +5,9 @@ export interface DeploymentTarget extends BaseModel, Named {
   name: string;
   type: string;
   geolocation?: Geolocation;
+  currentStatus?: DeploymentTargetStatus;
+}
+
+export interface DeploymentTargetStatus extends BaseModel {
+  message: string;
 }
