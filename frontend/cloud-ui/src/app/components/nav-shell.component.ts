0e3112185754d25bc953d5c54aef808cc1a62071
Jakob Steiner <kosmoz@users.noreply.github.com>
2024-12-10 12:23:13 +0000

    fix: toggle sidebar on tiny screens (#96)
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- frontend/cloud-ui/src/app/components/nav-shell.component.ts
+++ frontend/cloud-ui/src/app/components/nav-shell.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {NavBarComponent} from './nav-bar/nav-bar.component';
 import {SideBarComponent} from './side-bar/side-bar.component';
-import {RouterModule, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-nav-shell',
