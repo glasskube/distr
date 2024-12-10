79140c7bc9efe34e1af87043fbec46fbdb793e22
christophenne <christoph.enne@glasskube.eu>
2024-12-10 15:40:07 +0100

    feat(frontend): toasts only for 500
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- frontend/cloud-ui/src/app/app.config.ts
+++ frontend/cloud-ui/src/app/app.config.ts
@@ -1,18 +1,17 @@
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {ApplicationConfig, inject, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
-import {AuthService, tokenInterceptor} from './services/auth.service';
-import {errorInterceptor} from './services/error.interceptor';
+import {tokenInterceptor} from './services/auth.service';
+import {errorToastInterceptor} from './services/error-toast.interceptor';
 import {provideToastr} from 'ngx-toastr';
-import {ToastComponent} from './components/toast.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([tokenInterceptor, errorInterceptor])),
+    provideHttpClient(withInterceptors([tokenInterceptor, errorToastInterceptor])),
     provideAnimationsAsync(),
     provideToastr(),
   ],
