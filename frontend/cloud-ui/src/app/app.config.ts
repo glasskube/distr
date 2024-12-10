fb7b58beba28b0924da17b1be6264c7b0e5d9a2a
christophenne <christoph.enne@glasskube.eu>
2024-12-10 16:40:55 +0100

    feat(frontend): integrate sentry
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- frontend/cloud-ui/src/app/app.config.ts
+++ frontend/cloud-ui/src/app/app.config.ts
@@ -1,12 +1,34 @@
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {ApplicationConfig, inject, provideZoneChangeDetection} from '@angular/core';
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  ErrorHandler,
+  inject,
+  provideAppInitializer,
+  provideZoneChangeDetection
+} from '@angular/core';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {provideRouter} from '@angular/router';
+import {provideRouter, Router} from '@angular/router';
 import {routes} from './app.routes';
 import {AuthService, tokenInterceptor} from './services/auth.service';
+import * as Sentry from "@sentry/angular";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler(),
+    },
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER, // TODO ??
+      useFactory: () => () => {},
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(withInterceptors([tokenInterceptor])),
