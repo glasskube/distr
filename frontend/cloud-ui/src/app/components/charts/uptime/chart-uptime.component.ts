dd369aca88dafff79dd6d7c9537ffe918dffd037
Philip Miglinci <pmig@glasskube.com>
2024-12-11 15:18:56 +0100

    chore: delete unused functions and duplicate buttons
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>

--- frontend/cloud-ui/src/app/components/charts/uptime/chart-uptime.component.ts
+++ frontend/cloud-ui/src/app/components/charts/uptime/chart-uptime.component.ts
@@ -74,19 +74,4 @@ export class ChartUptimeComponent {
       },
     };
   }
-
-  public generateData(baseval: number, count: number, yrange: any) {
-    var i = 0;
-    var series = [];
-    while (i < count) {
-      var x = Math.floor(Math.random() * (750 - 1 + 1)) + 1;
-      var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-      var z = Math.floor(Math.random() * (75 - 15 + 1)) + 15;
-
-      series.push([x, y, z]);
-      baseval += 86400000;
-      i++;
-    }
-    return series;
-  }
 }
