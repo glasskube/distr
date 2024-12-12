9b6899df3565efda55dc12e23194421f57ff2f15
Jakob Steiner <kosmoz@users.noreply.github.com>
2024-12-12 14:19:44 +0000

    fix(ui): move modal submit button and general modal improvements (#120)

--- frontend/cloud-ui/src/app/animations/dropdown.ts
+++ frontend/cloud-ui/src/app/animations/dropdown.ts
@@ -2,6 +2,6 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 
 export const dropdownAnimation = trigger('dropdown', [
   state('in', style({transform: 'rotateX(0)'})),
-  transition('void => *', [style({transform: 'rotateX(-90deg)'}), animate(100)]),
-  transition('* => void', [animate(100, style({transform: 'rotateX(-90deg)'}))]),
+  transition('void => *', [style({transform: 'rotateX(-90deg)'}), animate('100ms ease-out')]),
+  transition('* => void', [animate('100ms ease-in', style({transform: 'rotateX(-90deg)'}))]),
 ]);
