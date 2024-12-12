a00c5715b319987b2fcb3c8f35253eb2b23c629c
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-12-12 14:05:32 +0100

    chore(ui): add animation easing

--- frontend/cloud-ui/src/app/animations/modal.ts
+++ frontend/cloud-ui/src/app/animations/modal.ts
@@ -2,6 +2,6 @@ import {trigger, state, style, transition, animate} from '@angular/animations';
 
 export const modalFlyInOut = trigger('modalFlyInOut', [
   state('in', style({transform: 'translateY(0)', opacity: '1'})),
-  transition('void => *', [style({transform: 'translateY(-100%)', opacity: '0'}), animate(150)]),
-  transition('* => void', [animate(150, style({transform: 'translateY(-100%)', opacity: '0'}))]),
+  transition('void => *', [style({transform: 'translateY(-100%)', opacity: '0.8'}), animate('150ms ease-out')]),
+  transition('* => void', [animate('150ms ease-in', style({transform: 'translateY(-100%)', opacity: '0.8'}))]),
 ]);
