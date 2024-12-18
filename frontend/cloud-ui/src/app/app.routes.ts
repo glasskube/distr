db7174484974f4fa2c439682f2f541a075635f94
Jakob Steiner <jakob.steiner@glasskube.eu>
2024-12-18 14:21:45 +0100

    fix(ui): add guarding route by role and redirect / to deployments for customer

--- frontend/cloud-ui/src/app/app.routes.ts
+++ frontend/cloud-ui/src/app/app.routes.ts
@@ -1,4 +1,11 @@
-import {ActivatedRouteSnapshot, createUrlTreeFromSnapshot, Router, RouterStateSnapshot, Routes} from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  createUrlTreeFromSnapshot,
+  Router,
+  RouterStateSnapshot,
+  Routes,
+} from '@angular/router';
 import {DashboardPlaceholderComponent} from './components/dashboard-placeholder/dashboard-placeholder.component';
 import {ApplicationsPageComponent} from './applications/applications-page.component';
 import {inject} from '@angular/core';
@@ -12,6 +19,62 @@ import {DeploymentsPageComponent} from './deployments/deployments-page.component
 import {VerifyComponent} from './verify/verify.component';
 import {ForgotComponent} from './forgot/forgot.component';
 import {PasswordResetComponent} from './password-reset/password-reset.component';
+import {UserRole} from './types/user-account';
+
+const jwtParamRedirectGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
+  const auth = inject(AuthService);
+  const jwt = route.queryParamMap.get('jwt');
+  if (jwt === null) {
+    return true;
+  } else {
+    // TODO: flush crud service caches
+    auth.token = jwt;
+    const newtree = createUrlTreeFromSnapshot(route, [], null, null);
+    delete newtree.queryParams['jwt']; // prevent infinite loop
+    return newtree;
+  }
+};
+
+const jwtAuthGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  if (auth.isAuthenticated) {
+    if (auth.getClaims().password_reset) {
+      if (state.url === '/reset') {
+        return true;
+      } else {
+        return router.createUrlTree(['/reset']);
+      }
+    } else if (!auth.getClaims().email_verified) {
+      if (state.url === '/verify') {
+        return true;
+      } else {
+        return router.createUrlTree(['/verify']);
+      }
+    } else {
+      return true;
+    }
+  } else {
+    return router.createUrlTree(['/login']);
+  }
+};
+
+function requiredRoleGuard(userRole: UserRole): CanActivateFn {
+  return () => inject(AuthService).hasRole(userRole);
+}
+
+const baseRoteRedirectGuard: CanActivateFn = () => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  switch (auth.getClaims().role) {
+    case 'customer':
+      return router.createUrlTree(['/deployments']);
+    case 'vendor':
+      return router.createUrlTree(['/dashboard']);
+    default:
+      return false;
+  }
+};
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
@@ -19,46 +82,14 @@ export const routes: Routes = [
   {path: 'forgot', component: ForgotComponent},
   {
     path: '',
-    canActivate: [
-      (route: ActivatedRouteSnapshot) => {
-        const auth = inject(AuthService);
-        const jwt = route.queryParamMap.get('jwt');
-        if (jwt === null) {
-          return true;
-        } else {
-          // TODO: flush crud service caches
-          auth.token = jwt;
-          const newtree = createUrlTreeFromSnapshot(route, [], null, null);
-          delete newtree.queryParams['jwt']; // prevent infinite loop
-          return newtree;
-        }
-      },
-      (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-        const auth = inject(AuthService);
-        const router = inject(Router);
-        if (auth.isAuthenticated) {
-          if (auth.getClaims().password_reset) {
-            if (state.url === '/reset') {
-              return true;
-            } else {
-              return router.createUrlTree(['/reset']);
-            }
-          } else if (!auth.getClaims().email_verified) {
-            if (state.url === '/verify') {
-              return true;
-            } else {
-              return router.createUrlTree(['/verify']);
-            }
-          } else {
-            return true;
-          }
-        } else {
-          return router.createUrlTree(['/login']);
-        }
-      },
-    ],
+    canActivate: [jwtParamRedirectGuard, jwtAuthGuard],
     children: [
-      {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
+      {
+        path: '',
+        pathMatch: 'full',
+        canActivate: [baseRoteRedirectGuard],
+        children: [],
+      },
       {path: 'verify', component: VerifyComponent},
       {path: 'reset', component: PasswordResetComponent},
       {path: 'join', component: InviteComponent},
@@ -66,11 +97,21 @@ export const routes: Routes = [
         path: '',
         component: NavShellComponent,
         children: [
-          {path: 'dashboard', component: DashboardPlaceholderComponent},
-          {path: 'applications', component: ApplicationsPageComponent},
+          {path: 'dashboard', component: DashboardPlaceholderComponent, canActivate: [requiredRoleGuard('vendor')]},
+          {path: 'applications', component: ApplicationsPageComponent, canActivate: [requiredRoleGuard('vendor')]},
           {path: 'deployments', component: DeploymentsPageComponent},
-          {path: 'customers', component: UsersComponent, data: {userRole: 'customer'}},
-          {path: 'users', component: UsersComponent, data: {userRole: 'vendor'}},
+          {
+            path: 'customers',
+            component: UsersComponent,
+            data: {userRole: 'customer'},
+            canActivate: [requiredRoleGuard('vendor')],
+          },
+          {
+            path: 'users',
+            component: UsersComponent,
+            data: {userRole: 'vendor'},
+            canActivate: [requiredRoleGuard('vendor')],
+          },
         ],
       },
     ],
