81311db0f217d5b7f17568acb26c837e6e53cfed
Jakob Steiner <kosmoz@users.noreply.github.com>
2024-12-20 15:01:04 +0000

    fix(ui): add height to charts (#202)

--- frontend/cloud-ui/src/app/components/charts/type/chart-type.component.ts
+++ frontend/cloud-ui/src/app/components/charts/type/chart-type.component.ts
@@ -1,26 +1,16 @@
-import {Component, inject, OnDestroy, ViewChild} from '@angular/core';
-import {ApexOptions, ChartComponent, NgApexchartsModule} from 'ng-apexcharts';
+import {Component, inject, OnDestroy} from '@angular/core';
+import {ApexOptions, NgApexchartsModule} from 'ng-apexcharts';
+import {Subject, takeUntil} from 'rxjs';
 import {DeploymentTargetsService} from '../../../services/deployment-targets.service';
-import {map, Subject, takeUntil} from 'rxjs';
 import {UserAccountWithRole} from '../../../types/user-account';
 
 @Component({
   selector: 'app-chart-type',
   templateUrl: './chart-type.component.html',
-  styles: [
-    `
-      #chart {
-        max-width: 100%;
-        max-height: 100%;
-        margin: 0 auto;
-      }
-    `,
-  ],
   imports: [NgApexchartsModule],
 })
 export class ChartTypeComponent implements OnDestroy {
-  @ViewChild('chart') chart!: ChartComponent;
-  public chartOptions: ApexOptions;
+  public chartOptions?: ApexOptions;
 
   private readonly deploymentTargets = inject(DeploymentTargetsService);
   private readonly deploymentTargets$ = this.deploymentTargets.list();
@@ -28,48 +18,48 @@ export class ChartTypeComponent implements OnDestroy {
   private readonly destroyed$ = new Subject<void>();
 
   constructor() {
-    this.chartOptions = {
-      series: [],
-      labels: [],
-      colors: ['#0db7ed', '#326CE5', '#174c76'],
-      chart: {
-        height: 192,
-        type: 'donut',
-        sparkline: {
-          enabled: true,
-        },
-      },
-      stroke: {
-        curve: 'smooth',
-      },
-      tooltip: {
-        enabled: false,
-      },
-      legend: {
-        show: true,
-        position: 'top',
-        fontFamily: 'Inter',
-        labels: {
-          colors: 'rgb(156, 163, 175)',
-          useSeriesColors: false,
-        },
-      },
-      title: {
-        text: 'Deployment Managers',
-        align: 'center',
-        offsetY: 10,
-        style: {
-          color: 'rgb(156, 163, 175)',
-          fontFamily: 'Poppins',
-        },
-      },
-      plotOptions: {
-        pie: {
-          customScale: 0.8,
-        },
-      },
-    };
     this.deploymentTargets$.pipe(takeUntil(this.destroyed$)).subscribe((dts) => {
+      this.chartOptions = {
+        series: [],
+        labels: [],
+        colors: ['#0db7ed', '#326CE5', '#174c76'],
+        chart: {
+          height: 192,
+          type: 'donut',
+          sparkline: {
+            enabled: true,
+          },
+        },
+        stroke: {
+          curve: 'smooth',
+        },
+        tooltip: {
+          enabled: false,
+        },
+        legend: {
+          show: true,
+          position: 'top',
+          fontFamily: 'Inter',
+          labels: {
+            colors: 'rgb(156, 163, 175)',
+            useSeriesColors: false,
+          },
+        },
+        title: {
+          text: 'Deployment Managers',
+          align: 'center',
+          offsetY: 10,
+          style: {
+            color: 'rgb(156, 163, 175)',
+            fontFamily: 'Poppins',
+          },
+        },
+        plotOptions: {
+          pie: {
+            customScale: 0.8,
+          },
+        },
+      };
       const managers: {[key: string]: UserAccountWithRole} = {};
       const counts: {[key: string]: number} = {};
       for (const dt of dts) {
@@ -80,6 +70,7 @@ export class ChartTypeComponent implements OnDestroy {
           counts[dt.createdBy.id] = counts[dt.createdBy.id] + 1;
         }
       }
+
       this.chartOptions.labels = Object.values(managers).map((v) => v.name ?? v.email);
       this.chartOptions.series = Object.values(counts);
     });
