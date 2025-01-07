b54024889b4bdf6d78a6b51ea9b667263fd877dc
christophenne <christoph.enne@glasskube.eu>
2025-01-07 15:40:35 +0100

    WIP: charts
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- frontend/cloud-ui/src/app/components/charts/type/chart-type.component.ts
+++ frontend/cloud-ui/src/app/components/charts/type/chart-type.component.ts
@@ -17,62 +17,59 @@ export class ChartTypeComponent implements OnDestroy {
 
   private readonly destroyed$ = new Subject<void>();
 
+  loading: boolean = true;
+
   constructor() {
     this.deploymentTargets$.pipe(takeUntil(this.destroyed$)).subscribe((dts) => {
-      this.chartOptions = {
-        series: [],
-        labels: [],
-        colors: ['#0db7ed', '#326CE5', '#174c76'],
-        chart: {
-          height: 192,
-          type: 'donut',
-          sparkline: {
-            enabled: true,
-          },
-        },
-        stroke: {
-          curve: 'smooth',
-        },
-        tooltip: {
-          enabled: false,
-        },
-        legend: {
-          show: true,
-          position: 'top',
-          fontFamily: 'Inter',
-          labels: {
-            colors: 'rgb(156, 163, 175)',
-            useSeriesColors: false,
-          },
-        },
-        title: {
-          text: 'Deployment Managers',
-          align: 'center',
-          offsetY: 10,
-          style: {
-            color: 'rgb(156, 163, 175)',
-            fontFamily: 'Poppins',
-          },
-        },
-        plotOptions: {
-          pie: {
-            customScale: 0.8,
-          },
-        },
-      };
-      const managers: {[key: string]: UserAccountWithRole} = {};
-      const counts: {[key: string]: number} = {};
-      for (const dt of dts) {
-        if (dt.createdBy?.id && !managers[dt.createdBy.id]) {
-          managers[dt.createdBy.id] = dt.createdBy;
-          counts[dt.createdBy.id] = 1;
-        } else if (dt.createdBy?.id && managers[dt.createdBy.id]) {
-          counts[dt.createdBy.id] = counts[dt.createdBy.id] + 1;
-        }
-      }
+      this.loading = false;
 
-      this.chartOptions.labels = Object.values(managers).map((v) => v.name ?? v.email);
-      this.chartOptions.series = Object.values(counts);
+      if (dts.length > 0) {
+        this.chartOptions = {
+          series: [],
+          labels: [],
+          colors: ['#0db7ed', '#326CE5', '#174c76'],
+          chart: {
+            height: 192,
+            type: 'donut',
+            sparkline: {
+              enabled: true,
+            },
+          },
+          stroke: {
+            curve: 'smooth',
+          },
+          tooltip: {
+            enabled: false,
+          },
+          legend: {
+            show: true,
+            position: 'top',
+            fontFamily: 'Inter',
+            labels: {
+              colors: 'rgb(156, 163, 175)',
+              useSeriesColors: false,
+            },
+          },
+          plotOptions: {
+            pie: {
+              customScale: 0.8,
+            },
+          },
+        };
+        const managers: {[key: string]: UserAccountWithRole} = {};
+        const counts: {[key: string]: number} = {};
+        for (const dt of dts) {
+          if (dt.createdBy?.id && !managers[dt.createdBy.id]) {
+            managers[dt.createdBy.id] = dt.createdBy;
+            counts[dt.createdBy.id] = 1;
+          } else if (dt.createdBy?.id && managers[dt.createdBy.id]) {
+            counts[dt.createdBy.id] = counts[dt.createdBy.id] + 1;
+          }
+        }
+
+        this.chartOptions.labels = Object.values(managers).map((v) => v.name ?? v.email);
+        this.chartOptions.series = Object.values(counts);
+      }
     });
   }
 
