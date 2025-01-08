069327254d43ea717299e032c37360c6436ecb1f
Christoph Enne <christoph.enne@glasskube.eu>
2025-01-08 16:47:11 +0100

    feat(ui): make deployment target selectable in uptime chart (#236)
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- frontend/cloud-ui/src/app/components/charts/uptime/chart-uptime.component.ts
+++ frontend/cloud-ui/src/app/components/charts/uptime/chart-uptime.component.ts
@@ -1,84 +1,102 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {ApexOptions, NgApexchartsModule} from 'ng-apexcharts';
-import {firstValueFrom, lastValueFrom} from 'rxjs';
+import {firstValueFrom} from 'rxjs';
 import {DeploymentTargetsService} from '../../../services/deployment-targets.service';
 import {MetricsService} from '../../../services/metrics.service';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
+import {faEllipsis} from '@fortawesome/free-solid-svg-icons';
+import {CdkConnectedOverlay, CdkOverlayOrigin} from '@angular/cdk/overlay';
+import {dropdownAnimation} from '../../../animations/dropdown';
+import {AsyncPipe} from '@angular/common';
+import {DeploymentTarget} from '../../../types/deployment-target';
 
 @Component({
   selector: 'app-chart-uptime',
   templateUrl: './chart-uptime.component.html',
-  imports: [NgApexchartsModule],
+  imports: [NgApexchartsModule, FaIconComponent, CdkOverlayOrigin, CdkConnectedOverlay, AsyncPipe],
+  animations: [dropdownAnimation],
 })
 export class ChartUptimeComponent implements OnInit {
+  private readonly LOCAL_STORAGE_KEY = 'dashboard_uptime_deployment_target_id';
   public chartOptions?: ApexOptions;
 
   private readonly deploymentTargets = inject(DeploymentTargetsService);
-  private readonly deploymentTargets$ = this.deploymentTargets.list();
+  readonly deploymentTargets$ = this.deploymentTargets.list();
 
   private readonly metrics = inject(MetricsService);
 
+  showDropdown = false;
+  storedDeploymentTargetId?: string;
+  selectedDeploymentTarget?: DeploymentTarget;
+
+  protected readonly faEllipsis = faEllipsis;
+
   async ngOnInit() {
+    this.storedDeploymentTargetId = window.localStorage[this.LOCAL_STORAGE_KEY];
     const dts = await firstValueFrom(this.deploymentTargets$);
-    for (const dt of dts) {
-      if (dt.currentStatus) {
-        // temporarily: simply show uptime of the first deployment target with a status and a deployment
-        if (!dt.latestDeployment) {
-          continue;
-        }
-        this.metrics.getUptimeForDeployment(dt.latestDeployment.id!).subscribe((uptimes) => {
-          this.chartOptions = {
-            series: [
-              {
-                name: 'available',
-                data: uptimes.map((ut) => ut.total - ut.unknown),
-                color: '#00bfa5',
-              },
-              {
-                name: 'unknown',
-                data: uptimes.map((ut) => ut.unknown),
-                color: '#feb019',
-              },
-            ],
-            chart: {
-              height: 220,
-              type: 'bar',
-              stacked: true,
-              sparkline: {
-                enabled: true,
-              },
-            },
-            stroke: {
-              curve: 'smooth',
-            },
-            tooltip: {
-              enabled: false,
-            },
-            xaxis: {
-              type: 'datetime',
-              categories: uptimes.map((ut) => ut.hour),
-            },
-            legend: {
-              show: true,
-              position: 'top',
-              fontFamily: 'Inter',
-              offsetY: -18,
-              labels: {
-                colors: 'rgb(156, 163, 175)',
-                useSeriesColors: false,
-              },
-            },
-            title: {
-              text: `${dt.name}`,
-              align: 'center',
-              style: {
-                color: 'rgb(156, 163, 175)',
-                fontFamily: 'Poppins',
-              },
-            },
-          };
-        });
-        return;
-      }
+    let initiallySelected = dts.find((dt) => dt.id === this.storedDeploymentTargetId);
+    if (!initiallySelected && dts.length > 0) {
+      initiallySelected = dts[0];
+    }
+    if (initiallySelected) {
+      await this.selectDeploymentTarget(initiallySelected);
+    } else {
+      delete window.localStorage[this.LOCAL_STORAGE_KEY];
+    }
+  }
+
+  async selectDeploymentTarget(dt: DeploymentTarget) {
+    this.showDropdown = false;
+    this.selectedDeploymentTarget = dt;
+    window.localStorage[this.LOCAL_STORAGE_KEY] = dt.id;
+    this.chartOptions = undefined;
+    if (dt.latestDeployment?.id) {
+      const uptimes = await firstValueFrom(this.metrics.getUptimeForDeployment(dt.latestDeployment.id));
+      this.chartOptions = {
+        series: [
+          {
+            name: 'available',
+            data: uptimes.map((ut) => ut.total - ut.unknown),
+            color: '#00bfa5',
+          },
+          {
+            name: 'unknown',
+            data: uptimes.map((ut) => ut.unknown),
+            color: '#feb019',
+          },
+        ],
+        chart: {
+          offsetY: 10,
+          //width: '100%',
+          //height: '80%',
+          type: 'bar',
+          stacked: true,
+          sparkline: {
+            enabled: true,
+          },
+        },
+        stroke: {
+          curve: 'smooth',
+        },
+        tooltip: {
+          enabled: false,
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: uptimes.map((ut) => ut.hour),
+        },
+        legend: {
+          show: true,
+          position: 'top',
+          fontFamily: 'Inter',
+          offsetY: -5,
+          floating: true,
+          labels: {
+            colors: 'rgb(156, 163, 175)',
+            useSeriesColors: false,
+          },
+        },
+      };
     }
   }
 }
