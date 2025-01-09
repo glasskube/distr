6db6393ce107df9938e0d0f1bf8feccb06cd515b
christophenne <christoph.enne@glasskube.eu>
2025-01-09 08:18:01 +0100

    fix: down migrations
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- internal/migrations/sql/1_helm_deployments.down.sql
+++ internal/migrations/sql/1_helm_deployments.down.sql
@@ -1,8 +1,8 @@
 ALTER TABLE ApplicationVersion
-  DROP COLUMN chart_type HELM_CHART_TYPE,
-  DROP COLUMN chart_name TEXT,
-  DROP COLUMN chart_url TEXT,
-  DROP COLUMN chart_version TEXT,
+  DROP COLUMN chart_type,
+  DROP COLUMN chart_name,
+  DROP COLUMN chart_url,
+  DROP COLUMN chart_version,
   DROP COLUMN values_file_data,
   DROP COLUMN template_file_data;
 
