88c1b0f52cb5beced1cc3a5910c90d46af947024
christophenne <christoph.enne@glasskube.eu>
2025-01-15 09:33:16 +0100

    [WIP] down migration
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- internal/migrations/sql/6_deployment_revision.down.sql
+++ internal/migrations/sql/6_deployment_revision.down.sql
@@ -1,1 +1,13 @@
 -- TODO
+
+DROP TABLE IF EXISTS DeploymentRevision CASCADE;
+
+-- TODO ??
+DELETE FROM Deployment;
+
+ALTER TABLE Deployment
+  ADD COLUMN application_version_id UUID NOT NULL REFERENCES ApplicationVersion (id) ON DELETE CASCADE,
+  ADD COLUMN values_yaml BYTEA;
+
+ALTER TABLE Deployment
+  DROP CONSTRAINT release_name_unique;
