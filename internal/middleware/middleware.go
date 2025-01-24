f618996e21c5aba9eeaa7fff01b206f9c250b5d4
christophenne <christoph.enne@glasskube.eu>
2025-01-24 10:02:23 +0100

    chore: additional api request limits
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -13,7 +13,6 @@ import (
 	"github.com/glasskube/cloud/internal/mail"
 	"github.com/glasskube/cloud/internal/types"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/httprate"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"go.uber.org/zap"
 )
@@ -88,17 +87,13 @@ func SentryUser(h http.Handler) http.Handler {
 	})
 }
 
-var RateLimitPerUser = httprate.Limit(
-	3,
-	10*time.Minute,
-	httprate.WithKeyFuncs(func(r *http.Request) (string, error) {
-		if auth, err := auth.Authentication.Get(r.Context()); err != nil {
-			return "", err
-		} else {
-			return auth.CurrentUserID(), nil
-		}
-	}),
-)
+func RateLimitCurrentUserIdKeyFunc(r *http.Request) (string, error) {
+	if auth, err := auth.Authentication.Get(r.Context()); err != nil {
+		return "", err
+	} else {
+		return auth.CurrentUserID(), nil
+	}
+}
 
 var RequireOrgID = auth.Authentication.ValidatorMiddleware(func(value authinfo.AuthInfo) error {
 	if value.CurrentOrgID() == nil {
