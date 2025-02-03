6a031f62f34d7476df0f1917ce9e0d28f14c6a75
christophenne <christoph.enne@glasskube.eu>
2025-02-03 14:14:56 +0100

    fix(ui): simplify
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- frontend/ui/src/app/app.component.ts
+++ frontend/ui/src/app/app.component.ts
@@ -30,8 +30,7 @@ export class AppComponent implements OnInit {
 
   public ngOnInit() {
     this.navigationEnd$.subscribe(() => {
-      const claims = this.auth.getClaims();
-      const email = claims?.email;
+      const email = this.auth.getClaims()?.email;
       Sentry.setUser({email});
       posthog.setPersonProperties({email});
       posthog.capture('$pageview');
