693131e38535d2efef5a955944d6b6ca841e99b9
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-02-13 16:44:28 +0100

    chore: show version from application if empty in license
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- frontend/ui/src/app/services/license.service.ts
+++ frontend/ui/src/app/services/license.service.ts
@@ -11,7 +11,9 @@ export class LicenseService {
           id: 'ced3992d-6717-498e-8726-6845ec3069b4',
           name: 'test',
           applicationId,
-          versions: [{id: 'bf58c94e-0c5b-476a-a647-09464d85bcae', name: 'v4.2.0'}],
+          versions: [
+            // {id: 'bf58c94e-0c5b-476a-a647-09464d85bcae', name: 'v4.2.0'}
+          ],
         },
       ],
     ]);
