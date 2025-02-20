aca08af92f61343c7bd6eaef800a65cd4c6fe8bc
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-02-20 12:27:16 +0100

    fix(ui): make autotrim work with FormControls
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- frontend/ui/src/app/directives/autotrim.directive.ts
+++ frontend/ui/src/app/directives/autotrim.directive.ts
@@ -1,11 +1,17 @@
-import {Directive, ElementRef, HostListener, inject} from '@angular/core';
+import {Directive, HostListener, inject} from '@angular/core';
+import {NgControl} from '@angular/forms';
 
 @Directive({selector: 'input[autotrim]'})
 export class AutotrimDirective {
+  private readonly ngControl = inject(NgControl, {optional: true});
+
   @HostListener('blur', ['$event']) onBlur(event: Event) {
     const target = event.target as HTMLInputElement;
     if (target.value !== target.value.trim()) {
       target.value = target.value.trim();
+    }
+    if (typeof this.ngControl?.value === 'string' && this.ngControl.value != this.ngControl.value.trim()) {
+      this.ngControl.control?.setValue(this.ngControl.value.trim());
     }
   }
 }
