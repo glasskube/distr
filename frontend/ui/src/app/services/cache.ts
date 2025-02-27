10f1b341229001e65d5e68003c839769437191e9
christophenne <christoph.enne@glasskube.eu>
2025-02-27 15:46:53 +0100

    chore(ui): make cache resettable
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- frontend/ui/src/app/services/cache.ts
+++ frontend/ui/src/app/services/cache.ts
@@ -1,46 +1,71 @@
-import {combineLatest, map, Observable, scan, shareReplay, startWith, Subject} from 'rxjs';
+import {concat, map, Observable, scan, shareReplay, Subject} from 'rxjs';
 import {compareBy, distinctBy, Predicate} from '../../util/arrays';
 import {BaseModel, Named} from '@glasskube/distr-sdk';
+
+interface ReactiveListEvent<T> {
+  objects?: T[]; // used for type 'reset'
+  object?: T; // used when type is 'save' or 'remove'
+  type: ReactiveListEventType;
+}
+
+type ReactiveListEventType = 'save' | 'remove' | 'reset';
 
 export abstract class ReactiveList<T> {
   protected abstract readonly identify: Predicate<T, unknown>;
   protected abstract readonly sortAttr: Predicate<T, string>;
 
-  private readonly saved$ = new Subject<T[]>();
-  private readonly savedAcc$: Observable<T[]> = this.saved$.pipe(
-    scan((list: T[], it: T[]) => distinctBy(this.identify)([...it, ...list]), []),
-    startWith([])
-  );
-
-  private readonly removed$ = new Subject<T>();
-  private readonly removedIdsAcc$: Observable<Set<unknown>> = this.removed$.pipe(
-    map((it) => this.identify(it)),
-    scan((set: Set<unknown>, it: unknown) => set.add(it), new Set()),
-    startWith(new Set())
-  );
-
-  private readonly actual$: Observable<T[]>;
+  private readonly events$ = new Subject<ReactiveListEvent<T>>();
+  private readonly state$: Observable<T[]>;
 
   constructor(private readonly initial$: Observable<T[]>) {
-    this.actual$ = combineLatest([this.initial$, this.savedAcc$, this.removedIdsAcc$]).pipe(
-      map(([initialLs, savedLs, removedIds]) =>
-        distinctBy(this.identify)([...savedLs, ...initialLs]).filter((it) => !removedIds.has(this.identify(it)))
-      ),
+    // TODO potential race condition: initial load takes too long and other stuff is being added locally
+    // could be prevented locally by requiring the first event to always be of reset type and otherwise not pushing the event?
+    this.state$ = concat(
+      this.initial$.pipe(map((items) => ({type: 'reset', objects: items}) as ReactiveListEvent<T>)),
+      this.events$
+    ).pipe(
+      scan((state: T[], event: ReactiveListEvent<T>) => {
+        if (event.type === 'reset' && event.objects) {
+          return event.objects;
+        } else if (event.type === 'save' && event.object) {
+          return distinctBy(this.identify)([event.object, ...state]);
+        } else if (event.type === 'remove' && event.object) {
+          return state.filter((item) => {
+            return this.identify(item) !== this.identify(event.object!);
+          });
+        } else {
+          console.warn('unsupported/invalid event: ' + event);
+          return state;
+        }
+      }, []),
       map((ls: T[]) => ls.sort(compareBy(this.sortAttr))),
       shareReplay(1)
     );
   }
 
-  public save(...arg: T[]) {
-    this.saved$.next(arg);
+  public save(arg: T) {
+    this.events$.next({
+      type: 'save',
+      object: arg,
+    });
   }
 
   public remove(arg: T) {
-    this.removed$.next(arg);
+    this.events$.next({
+      type: 'remove',
+      object: arg,
+    });
+  }
+
+  public reset(arg: T[]) {
+    this.events$.next({
+      type: 'reset',
+      objects: arg,
+    });
   }
 
   public get(): Observable<T[]> {
-    return this.actual$;
+    return this.state$;
   }
 }
 
