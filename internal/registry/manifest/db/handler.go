0081e5b08cee50b3088873c740115a6496416eda
Christoph Enne <christoph.enne@glasskube.eu>
2025-03-13 16:05:54 +0100

    feat: show registry artifacts in UI (#624)
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- internal/registry/manifest/db/handler.go
+++ internal/registry/manifest/db/handler.go
@@ -47,10 +47,10 @@ func (h *handler) Get(ctx context.Context, nameStr string, reference string) (*m
 // List implements manifest.ManifestHandler.
 func (h *handler) List(ctx context.Context, n int) ([]string, error) {
 	auth := auth.ArtifactsAuthentication.Require(ctx)
-	var artifacts []types.Artifact
+	var artifacts []types.ArtifactWithTaggedVersion
 	var err error
 	if *auth.CurrentUserRole() == types.UserRoleCustomer {
-		artifacts, err = db.GetArtifactsByLicenseOwnerID(ctx, auth.CurrentUserID())
+		artifacts, err = db.GetArtifactsByLicenseOwnerID(ctx, *auth.CurrentOrgID(), auth.CurrentUserID())
 	} else {
 		artifacts, err = db.GetArtifactsByOrgID(ctx, *auth.CurrentOrgID())
 	}
