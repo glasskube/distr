0e99ed057cc137303136cea0160d43ee05d7206c
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-03-26 16:21:33 +0100

    fix: use bigint for size columns
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- internal/migrations/sql/25_artifact_size.up.sql
+++ internal/migrations/sql/25_artifact_size.up.sql
@@ -1,4 +1,4 @@
-ALTER TABLE ArtifactVersion ADD COLUMN manifest_blob_size INTEGER NOT NULL DEFAULT 0;
-ALTER TABLE ArtifactVersionPart ADD COLUMN artifact_blob_size INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE ArtifactVersion ADD COLUMN manifest_blob_size BIGINT NOT NULL DEFAULT 0;
+ALTER TABLE ArtifactVersionPart ADD COLUMN artifact_blob_size BIGINT NOT NULL DEFAULT 0;
 ALTER TABLE ArtifactVersion ALTER COLUMN manifest_blob_size DROP DEFAULT;
 ALTER TABLE ArtifactVersionPart ALTER COLUMN artifact_blob_size DROP DEFAULT;
