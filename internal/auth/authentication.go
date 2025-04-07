a049958f84df74abecf414e06050b01ead373cb7
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-04-07 15:15:33 +0200

    feat: agent registry auth with JWT

--- internal/auth/authentication.go
+++ internal/auth/authentication.go
@@ -6,10 +6,8 @@ import (
 	"github.com/getsentry/sentry-go"
 	"github.com/glasskube/distr/internal/authjwt"
 	"github.com/glasskube/distr/internal/authn"
-	"github.com/glasskube/distr/internal/authn/agent"
 	"github.com/glasskube/distr/internal/authn/authinfo"
 	"github.com/glasskube/distr/internal/authn/authkey"
-	"github.com/glasskube/distr/internal/authn/basic"
 	"github.com/glasskube/distr/internal/authn/jwt"
 	"github.com/glasskube/distr/internal/authn/token"
 	internalctx "github.com/glasskube/distr/internal/context"
@@ -46,26 +44,24 @@ var AgentAuthentication = authn.New(
 // ArtifactsAuthentication supports Basic auth login for OCI clients, where the password should be a PAT.
 // The given PAT is verified against the database, to make sure that the user still exists.
 var ArtifactsAuthentication = authn.New(
-	authn.Alternative(
-		// Authenticate with Agent JWT
-		authn.Chain3(
-			token.NewExtractor(token.WithExtractorFuncs(token.FromHeader("Bearer"))),
-			jwt.Authenticator(authjwt.JWTAuth),
-			authinfo.AgentJWTAuthenticator(),
+	authn.Chain(
+		token.NewExtractor(
+			token.WithExtractorFuncs(token.FromBasicAuth()),
+			token.WithErrorHeaders(http.Header{"WWW-Authenticate": []string{"Basic realm=\"Distr\""}}),
 		),
-		// Auhtenticate UserAccount with PAT via BasicAuth
-		authn.Chain3(
-			token.NewExtractor(
-				token.WithExtractorFuncs(token.FromBasicAuth()),
-				token.WithErrorHeaders(http.Header{"WWW-Authenticate": []string{"Basic realm=\"Distr\""}}),
+		authn.Alternative(
+			// Auhtenticate UserAccount with PAT
+			authn.Chain3(
+				authkey.Authenticator(),
+				authinfo.AuthKeyAuthenticator(),
+				authinfo.DbAuthenticator(),
 			),
-			authkey.Authenticator(),
-			authinfo.AuthKeyAuthenticator(),
-		),
-		// Authenticate Agent with ID and secret via BasicAuth
-		authn.Chain(
-			basic.Authenticator(),
-			agent.Authenticator(),
+			// Authenticate with Agent JWT
+			authn.Chain3(
+				jwt.Authenticator(authjwt.JWTAuth),
+				authinfo.AgentJWTAuthenticator(),
+				authinfo.AgentDbAuthenticator(),
+			),
 		),
 	),
 )
--- internal/util/pointer.go
+++ internal/util/pointer.go
@@ -3,3 +3,10 @@ package util
 func PtrTo[T any](value T) *T {
 	return &value
 }
+
+func PtrCopy[T any](ptr *T) *T {
+	if ptr == nil {
+		return nil
+	}
+	return &*ptr
+}
