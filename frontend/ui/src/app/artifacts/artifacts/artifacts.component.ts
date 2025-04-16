c97f0241cd9098099cf5626c59632d9ec6679024
Philip Miglinci <pmig@glasskube.com>
2025-04-16 11:55:35 +0200

    chore(ui): don't display push instructions for non vendors
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>

--- frontend/ui/src/app/artifacts/artifacts/artifacts.component.ts
+++ frontend/ui/src/app/artifacts/artifacts/artifacts.component.ts
@@ -3,16 +3,16 @@ import {Component, inject} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {RouterLink} from '@angular/router';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
-import {faBox, faDownload, faLightbulb, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
+import {faBox, faLightbulb, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 import {combineLatest, map, startWith} from 'rxjs';
 import {UuidComponent} from '../../components/uuid';
 import {ArtifactsService} from '../../services/artifacts.service';
 import {ArtifactsDownloadCountComponent, ArtifactsDownloadedByComponent} from '../components';
-import {faDocker} from '@fortawesome/free-brands-svg-icons';
 import {AutotrimDirective} from '../../directives/autotrim.directive';
 import {getRemoteEnvironment} from '../../../env/remote';
 import {fromPromise} from 'rxjs/internal/observable/innerFrom';
 import {OrganizationService} from '../../services/organization.service';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-artifacts',
@@ -33,7 +33,6 @@ export class ArtifactsComponent {
 
   protected readonly faMagnifyingGlass = faMagnifyingGlass;
   protected readonly faBox = faBox;
-  protected readonly faDownload = faDownload;
 
   protected readonly filterForm = new FormGroup({
     search: new FormControl(''),
@@ -48,10 +47,12 @@ export class ArtifactsComponent {
       artifacts.filter((it) => !formValue.search || it.name.toLowerCase().includes(formValue.search.toLowerCase()))
     )
   );
-  protected readonly faDocker = faDocker;
   protected readonly faLightbulb = faLightbulb;
 
   private readonly organizationService = inject(OrganizationService);
   protected readonly registrySlug$ = this.organizationService.get().pipe(map((o) => o.slug));
   protected readonly registryHost$ = fromPromise(getRemoteEnvironment()).pipe(map((e) => e.registryHost));
+
+  private readonly authService = inject(AuthService);
+  protected readonly isVendor = this.authService.hasRole('vendor');
 }
