54596dd186da267befbc48053e614b0392542e35
christophenne <christoph.enne@glasskube.eu>
2025-04-30 13:25:42 +0200

    chore: remove uptime metric
    Signed-off-by: christophenne <christoph.enne@glasskube.eu>

--- sdk/js/src/types/index.ts
+++ sdk/js/src/types/index.ts
@@ -6,5 +6,4 @@ export * from './deployment';
 export * from './deployment-target';
 export * from './geolocation';
 export * from './organization-branding';
-export * from './uptime';
 export * from './user-account';
--- sdk/js/src/types/uptime.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @deprecated this will be removed in v2.
- */
-export interface UptimeMetric {
-  hour: string;
-  total: number;
-  unknown: number;
-}
