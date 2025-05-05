cdcda7aa73efc3f201b4183a150b155412e9c00d
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-05-05 14:09:25 +0200

    fix(ui): improve markdown rendering on customer "home" page

--- frontend/ui/src/app/services/markdown-options.factory.ts
+++ frontend/ui/src/app/services/markdown-options.factory.ts
@@ -1,29 +1,14 @@
 import {MarkedOptions, MarkedRenderer} from 'ngx-markdown';
+import {parseInline} from 'marked';
 
 export function markedOptionsFactory(): MarkedOptions {
   const renderer = new MarkedRenderer();
-
-  renderer.heading = (h) => {
-    const xs = 6 - h.depth;
-    const textClass = h.depth <= 4 ? `text-${xs}xl` : h.depth === 5 ? 'text-xl' : 'text-lg';
-    const fontClass = h.depth === 1 ? 'font-extrabold' : 'font-bold';
-    return `<h${h.depth} class='${textClass} ${fontClass} my-${xs}'>${h.text}</h${h.depth}>`;
-  };
-
-  renderer.code = (code) => {
-    return `
-        <pre>
-          <code class="text-gray-900 dark:text-gray-200 whitespace-pre-line">
-              ${code.text}
-          </code>
-        </pre>`;
-  };
+  const opts: MarkedOptions = {renderer: renderer};
 
   renderer.link = (link) => {
-    return `<a href="${link.href}" class="text-primary-600 underline" target="_blank">${link.text}</a>`;
+    const text = parseInline(link.text, {...opts, async: false});
+    return `<a href="${link.href}" target="_blank">${text}</a>`;
   };
 
-  return {
-    renderer: renderer,
-  };
+  return opts;
 }
