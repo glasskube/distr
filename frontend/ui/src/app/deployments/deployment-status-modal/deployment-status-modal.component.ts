ce48bca1262d699217a41bee246724ab0c1451dc
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-05-16 16:23:15 +0200

    chore: move logs to status modal [WIP]

--- frontend/ui/src/app/deployments/deployment-status-modal/deployment-status-modal.component.ts
+++ frontend/ui/src/app/deployments/deployment-status-modal/deployment-status-modal.component.ts
@@ -1,9 +1,10 @@
-import {Component, inject, input, output} from '@angular/core';
+import {Component, inject, input, output, resource, signal} from '@angular/core';
 import {toObservable} from '@angular/core/rxjs-interop';
 import {DeploymentTarget, DeploymentWithLatestRevision} from '@glasskube/distr-sdk';
-import {distinctUntilChanged, filter, map, switchMap} from 'rxjs';
+import {distinctUntilChanged, filter, firstValueFrom, map, switchMap} from 'rxjs';
 import {DeploymentStatusService} from '../../services/deployment-status.service';
 import {AsyncPipe, DatePipe} from '@angular/common';
+import {DeploymentLogsService} from '../../services/deployment-logs.service';
 
 @Component({
   selector: 'app-deployment-status-modal',
@@ -12,6 +13,7 @@ import {AsyncPipe, DatePipe} from '@angular/common';
 })
 export class DeploymentStatusModalComponent {
   private readonly deploymentStatuses = inject(DeploymentStatusService);
+  private readonly deploymentLogs = inject(DeploymentLogsService);
 
   public readonly deploymentTarget = input.required<DeploymentTarget>();
   public readonly selectedDeployment = input.required<DeploymentWithLatestRevision>();
@@ -23,6 +25,16 @@ export class DeploymentStatusModalComponent {
     distinctUntilChanged(),
     switchMap((id) => this.deploymentStatuses.pollStatuses(id!))
   );
+  protected readonly resources = toObservable(this.selectedDeployment).pipe(
+    map((d) => d.id),
+    filter((id) => !!id),
+    distinctUntilChanged(),
+    switchMap((id) => this.deploymentLogs.getResources(id!))
+  );
+  /**
+   * `null` means agent status
+   */
+  protected readonly selectedResource = signal<string | null>(null);
 
   protected hideModal() {
     this.closed.emit();
