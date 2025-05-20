1bc70bf1db5b43eabb9c4e514ac1aaca042d2009
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-05-20 14:52:49 +0200

    chore(ui): refactor state changes in logs table component
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- frontend/ui/src/app/deployments/deployment-status-modal/deployment-logs-table.component.ts
+++ frontend/ui/src/app/deployments/deployment-status-modal/deployment-logs-table.component.ts
@@ -1,10 +1,10 @@
-import {Component, effect, inject, input} from '@angular/core';
-import {DeploymentLogsService} from '../../services/deployment-logs.service';
-import {delay, map, Observable, of, scan, startWith, Subject, switchMap, tap} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
+import {Component, inject, input} from '@angular/core';
 import {toObservable} from '@angular/core/rxjs-interop';
+import {map, Observable, of, scan, startWith, Subject, switchMap, tap} from 'rxjs';
+import {DeploymentLogsService} from '../../services/deployment-logs.service';
 import {DeploymentLogRecord} from '../../types/deployment-log-record';
 import {DeploymentStatusTableComponent, DeploymentStatusTableEntry} from './deployment-status-table.component';
-import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-deployment-logs-table',
@@ -16,7 +16,7 @@ import {AsyncPipe} from '@angular/common';
           <button
             type="button"
             class="py-2 px-3 flex items-center text-sm font-medium text-center text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-            (click)="showMore()">
+            (click)="showMore(logs)">
             Load more
           </button>
         </div>
@@ -28,44 +28,35 @@ import {AsyncPipe} from '@angular/common';
 export class DeploymentLogsTableComponent {
   private readonly svc = inject(DeploymentLogsService);
 
-  private currentOldest?: Date;
   protected hasMore = true;
-  private readonly showMore$ = new Subject<void>();
+  private readonly showMoreBefore$ = new Subject<Date>();
 
   public readonly deploymentId = input.required<string>();
   public readonly resource = input.required<string>();
   protected readonly logs$: Observable<DeploymentStatusTableEntry[]> = toObservable(this.resource).pipe(
-    delay(0), // needed because this has to run after the effect() below
     switchMap((resource) =>
       resource
-        ? this.showMore$.pipe(
+        ? this.showMoreBefore$.pipe(
             startWith(undefined),
-            switchMap(() => this.svc.get(this.deploymentId(), resource, this.currentOldest)),
+            switchMap((before) => this.svc.get(this.deploymentId(), resource, before)),
             tap((logs) => (this.hasMore = logs.length > 0)),
-            scan((acc: DeploymentLogRecord[], logs) => (acc ?? []).concat(logs)),
-            tap((logs) => {
-              logs.forEach((rec) => {
-                const ts = new Date(rec.timestamp);
-                if (!this.currentOldest || ts < this.currentOldest) {
-                  this.currentOldest = ts;
-                }
-              });
-            })
+            scan((acc: DeploymentLogRecord[], logs) => (acc ?? []).concat(logs))
           )
         : of([])
     ),
     map((logs) => logs.map((it) => ({date: it.timestamp, status: it.severity, detail: it.body})))
   );
 
-  constructor() {
-    effect(() => {
-      this.resource();
-      this.currentOldest = undefined;
-      this.hasMore = true;
+  protected showMore(logs: DeploymentStatusTableEntry[]) {
+    let before: Date | null = null;
+    logs.forEach((rec) => {
+      const d = new Date(rec.date);
+      if (before === null || d < before) {
+        before = d;
+      }
     });
-  }
-
-  protected showMore() {
-    this.showMore$.next();
+    if (before !== null) {
+      this.showMoreBefore$.next(before);
+    }
   }
 }
