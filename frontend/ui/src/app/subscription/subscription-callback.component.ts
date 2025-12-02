250d1a23dd93c6dbb3f026779273adc8fe63a10f
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-12-02 15:55:27 +0100

    fix(ui): add periodic refresh on subscription callback page when subscription type is still trial
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- frontend/ui/src/app/subscription/subscription-callback.component.ts
+++ frontend/ui/src/app/subscription/subscription-callback.component.ts
@@ -1,8 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, effect, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 import {faCheckCircle} from '@fortawesome/free-solid-svg-icons';
 import {RouterLink} from '@angular/router';
+import {OrganizationService} from '../services/organization.service';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {map} from 'rxjs';
 
 @Component({
   selector: 'app-subscription-callback',
@@ -10,5 +13,19 @@ import {RouterLink} from '@angular/router';
   imports: [CommonModule, FaIconComponent, RouterLink],
 })
 export class SubscriptionCallbackComponent {
+  private readonly organizationService = inject(OrganizationService);
+
   protected readonly faCheckCircle = faCheckCircle;
+
+  protected readonly isTrial = toSignal(
+    this.organizationService.get().pipe(map((org) => org.subscriptionType === 'trial'))
+  );
+
+  constructor() {
+    effect(() => {
+      if (this.isTrial()) {
+        setTimeout(() => location.reload(), 5000);
+      }
+    });
+  }
 }
