ebb8c6acd82425a692c89e2127e0e6efceb9572a
Jakob Steiner <kosmoz@users.noreply.github.com>
2025-12-02 13:49:15 +0100

    chore: redirect vendor to pricing if subscription has ended (#1439)
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>
    Co-authored-by: Philip Miglinci <pmig@glasskube.com>

--- frontend/ui/src/app/components/users/vendors/vendor-users.component.ts
+++ frontend/ui/src/app/components/users/vendors/vendor-users.component.ts
@@ -6,7 +6,7 @@ import {toSignal} from '@angular/core/rxjs-interop';
 import {AuthService} from '../../../services/auth.service';
 
 @Component({
-  template: `<section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 antialiased sm:ml-64">
+  template: `<section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 antialiased">
     <div class="mx-auto max-w-screen-2xl px-4 lg:px-12">
       <div class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
         <app-users (refresh)="refresh$.next()" [users]="users() ?? []" [userRole]="userRole" />
