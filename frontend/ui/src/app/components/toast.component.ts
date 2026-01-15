43bd5bee9a9412303a2f2c73fa11104db94df6d4
Jakob Steiner <jakob.steiner@glasskube.eu>
2026-01-15 15:29:22 +0100

    fix(ui): prevent text overflow in toast component
    Signed-off-by: Jakob Steiner <jakob.steiner@glasskube.eu>

--- frontend/ui/src/app/components/toast.component.ts
+++ frontend/ui/src/app/components/toast.component.ts
@@ -1,7 +1,7 @@
 import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 import {Component} from '@angular/core';
 import {FaIconComponent} from '@fortawesome/angular-fontawesome';
-import {faCheck, faCircleExclamation, faCircleInfo} from '@fortawesome/free-solid-svg-icons';
+import {faCheck, faCircleExclamation, faCircleInfo, faXmark} from '@fortawesome/free-solid-svg-icons';
 import {Toast} from 'ngx-toastr';
 
 @Component({
@@ -48,7 +48,7 @@ import {Toast} from 'ngx-toastr';
           </fa-icon>
         }
       }
-      <div class="ms-3 text-sm font-normal">
+      <div class="ms-3 text-sm font-normal overflow-hidden">
         {{ title }}
         @if (message) {
           :{{ message }}
@@ -61,14 +61,7 @@ import {Toast} from 'ngx-toastr';
         data-dismiss-target="#toast-danger"
         aria-label="Close">
         <span class="sr-only">Close</span>
-        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-        </svg>
+        <fa-icon [icon]="faXmark"></fa-icon>
       </button>
     </div>
   `,
@@ -128,4 +121,5 @@ export class ToastComponent extends Toast {
   protected readonly faCheck = faCheck;
   protected readonly faCircleExclamation = faCircleExclamation;
   protected readonly faCircleInfo = faCircleInfo;
+  protected readonly faXmark = faXmark;
 }
