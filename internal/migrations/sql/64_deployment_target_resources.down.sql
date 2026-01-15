89400d09c6810b7519e410129097408cfda4800d
Philip Miglinci <pmig@glasskube.com>
2026-01-15 10:14:53 +0100

    chore: order migrations
    Signed-off-by: Philip Miglinci <pmig@glasskube.com>

--- internal/migrations/sql/64_deployment_target_resources.down.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE DeploymentTarget
-  DROP COLUMN resources_cpu_request,
-  DROP COLUMN resources_memory_request,
-  DROP COLUMN resources_cpu_limit,
-  DROP COLUMN resources_memory_limit;
